02f76795689a67ed0dca5e10bbc504e247e98f9c
Joel-2021 <varughesejoel95@gmail.com>
2025-09-12 18:30:59 +0530

    -- fixes

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -16,7 +16,7 @@ export function ThemeToggle() {
     return (
         <Button
             variant="ghost"
-            size="icon"
+            size="icon" aria-label="theme toggle"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
             {theme === "dark" ? (
